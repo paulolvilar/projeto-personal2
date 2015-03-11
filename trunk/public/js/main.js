29ae2f42b4a33161e190ae5700cd63ef145e660b
danielvlr@gmail.com <danielvlr@gmail.com@54b1ab3b-2250-25b6-c703-c735aa1eb67c>
2015-03-11 23:25:05 +0000

    Commit de navegacao

--- trunk/public/js/main.js
+++ trunk/public/js/main.js
@@ -1,5 +1,6 @@
 angular.module('contatooh', ['ngRoute', 'ngResource'])
 .config(function($routeProvider) {
+// 
 $routeProvider.when('/contatos', {
 templateUrl: 'partials/contatos.html',
 controller: 'ContatosController'
@@ -12,5 +13,30 @@ $routeProvider.when('/contato', {
 templateUrl: 'partials/contato.html',
 controller: 'ContatoController'
 });
-$routeProvider.otherwise({redirectTo: '/contatos'});
+
+// Cadastro de Aluno
+$routeProvider.when('/alunos', {
+templateUrl: 'partials/alunos.html'
+});
+	
+$routeProvider.when('/aluno/:alunoId', {
+templateUrl: 'partials/aluno.html'
+});
+
+$routeProvider.when('/aluno', {
+templateUrl: 'partials/aluno.html'
+});
+
+// Cadastro de Anamnese
+$routeProvider.when('/anamneses', {
+	templateUrl: 'partials/anamneses.html'
+});
+
+$routeProvider.when('/anamnese', {
+	templateUrl: 'partials/anamnese.html'
+});
+
+
+// Apos Logar
+$routeProvider.otherwise({redirectTo: '/dashboard'});
 });
