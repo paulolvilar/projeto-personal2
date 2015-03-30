89b819c3712d88b23f04ddd7cd4d2189a9f48aff
paulolvilar@gmail.com <paulolvilar@gmail.com@54b1ab3b-2250-25b6-c703-c735aa1eb67c>
2015-03-30 01:23:43 +0000

    anamnese

--- trunk/app/models/aluno.js
+++ trunk/app/models/aluno.js
@@ -19,6 +19,60 @@ module.exports = function() {
 		},
 		sexo: {
 			type: String,
+		},
+
+		anamnese:{
+			estatura: {
+				type: String,
+			},
+			peso: {
+				type: String,
+			},
+			sistemaCardiovascular: {
+				type: String
+			},
+			sistemaRespiratorio: {
+				type: String
+			},
+			sistemaMusculoEsqueletico: {
+				type: String
+			},
+			colunaVertebral: {
+				type: String
+			},
+			pas: {
+				type: String
+			},
+			pad: {
+				type: String
+			},
+			fcr: {
+				type: String
+			},
+			fcm: {
+				type: String
+			},
+			asCondCardiaca: {
+				type: String
+			},
+			asDorPeito: {
+				type: String
+			},
+			asDorPeitoExercicio: {
+				type: String
+			},
+			asPerdeEquilibrio: {
+				type: String
+			},
+			asProblemaJuntas: {
+				type: String
+			},
+			asRemedioPressao: {
+				type: String
+			},
+			asRazaoNaoAtividade: {
+				type: String
+			}
 		}
 	});
 	return mongoose.model('Aluno', schema);
